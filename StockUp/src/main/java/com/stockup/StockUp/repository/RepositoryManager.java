377bfc176ece0b92bbad65dcee09d013035a478b
Cyrilzak02 <cyrilzakhia@hotmail.com>
2024-10-09 00:32:14 -0300

    Back-End Functionning

--- StockUp/src/main/java/com/stockup/StockUp/repository/RepositoryManager.java
+++ StockUp/src/main/java/com/stockup/StockUp/repository/RepositoryManager.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RepositoryManager extends JpaRepository<Manager , Integer> {
-    Optional<Manager> findByEmailAndPassword(String email, String password);
+    Optional<Manager> findOneByEmailAndPassword(String email, String password);
 }
