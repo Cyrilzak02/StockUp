419d7a8b3ccdeb00fd01c01ed2a7bdc7c6253b74
julioDomanski <julio.domanski@hotmail.com>
2024-10-14 21:09:58 -0300

    routing

--- StockUpFront/stockupfront/src/App.js
+++ StockUpFront/stockupfront/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login.js'
-import CadastrarProduto from './Pages/CadastrarProduto.js'
+import CadastrarProduto from './Pages/CadastrarProduto.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <CadastrarProduto></CadastrarProduto>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} /> 
+            <Route path="/cadastrar-produto" element={<CadastrarProduto />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
